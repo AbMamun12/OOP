7718f8256f788a1eb08c697470034642e79dbc82
Abdullah <abdullah.mamun8191@gmail.com>
2025-05-27 23:20:46 +0600

    ok

--- OOP/polyphormisom.dart
+++ OOP/polyphormisom.dart
@@ -1,36 +1,35 @@
-abstract class Khan{
+abstract class Khan {
   void printMyname();
-
-
 }
-class Sahrukhkhan extends Khan{
+
+class Sahrukhkhan extends Khan {
   @override
   void printMyname() {
     print('My name is khan');
   }
 }
 
-class Amirkhaen extends Khan{
+class Amirkhaen extends Khan {
   @override
   void printMyname() {
     print('My name is Amir kahn');
   }
-
 }
+
 class SalmanKhan extends Khan {
   @override
   void printMyname() {
     print('My name is Amir kahn');
   }
 }
+
 // এটাকে বলে instance create করা
-void main(){
+void main() {
   Khan a = Amirkhaen();
-  Khan b =Sahrukhkhan();
+  Khan b = Sahrukhkhan();
   Khan c = SalmanKhan();
 
   a.printMyname();
   b.printMyname();
-   c.printMyname();
-
+  c.printMyname();
 }
