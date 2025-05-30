8f26ee6ece0aef3128d5376362f56d53a5a53123
Abdullah <abdullah.mamun8191@gmail.com>
2025-05-30 11:34:51 +0600

    ok

---
+++ OOP/6.2/mixins.dart
@@ -0,0 +1,32 @@
+class A{
+  void doSomething(){
+    print( 'Doing Nothing');
+
+  }
+}
+class B{
+  void anotherUselessMethod(){
+    print( 'Dont use');
+
+  }
+}
+
+class Worker with F,E {
+
+}
+
+mixin F{
+  void doSomething(){
+    print( 'Doing Nothing');
+
+  }
+
+}
+mixin E{
+  void doSomething(){
+    print( 'Doing Nothing');
+
+  }
+
+}
+
